2ba28f3cce4baefd2b8d80a0235c6b712a5d432d
Max <max@stoerchle.at>
2015-04-07 13:12:14 +0200

    weitergemacht

--- parseShanghaiRanking.rb
+++ parseShanghaiRanking.rb
@@ -1,6 +1,48 @@
+#encoding: UTF-8
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+
+def getTuition(universityname)
+  unistring = universityname.tr(' ', '+')
+  link = 'https://www.google.at/search?q=' + unistring + '+tuition+total+per+year'
+  page = Nokogiri::HTML(open(link))
+  text = page.css('#search').text.force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+  return text.match(/[\$\€\£]\d+[\,\.]\d+/).to_s
+end
+
+def convertToDollar(value)
+  if value.length > 0
+    if value[0] == '$'
+      return value
+    else
+      link = 'https://www.google.at/search?q=' + value + ' to $'
+      page = Nokogiri::HTML(open(URI.escape(link)))
+      text = page.css('.vk_ans>span').text.force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+      return "$" + text
+    end
+  else
+    return "$0"
+  end
+end
+
+def outputUniversity(university, *keys)
+  if keys.length > 0
+    keys.each do |key|
+      out = university[key.to_sym]
+      print(out.length == 0 ? "X" : out)
+      STDOUT.flush
+      if key == keys.last
+        puts ""
+      else
+        print " | "
+        STDOUT.flush
+      end
+    end
+  else
+    puts "wrong Parameters in outputUniversity"
+  end
+end
 
 link = 'http://www.shanghairanking.com/SubjectCS2014.html'
 page = Nokogiri::HTML(open(link))
@@ -21,17 +63,13 @@ allrows.each do |row|
   university[:hici] = cols[6].css('div').text
   university[:pub] = cols[7].css('div').text
   university[:top] = cols[8].css('div').text
+  university[:tuition] = convertToDollar(getTuition(university[:name]))
+  outputUniversity(university, 'rank', 'name', 'tuition')
   universities.push(university)
 end
+
 
 # universities.each do |u|
 #   puts u[:rank] + ' | ' + u[:name]
 # end
-
-testuni = universities[0]
-unistring = testuni[:name].tr(' ', '+')
-link = 'https://www.google.at/search?q=' + unistring + '+tuition+total+per+year'
-puts link
-page = Nokogiri::HTML(open(link))
-puts page.text
 
