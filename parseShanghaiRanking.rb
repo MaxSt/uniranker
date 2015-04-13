8d8afa030b8793ec624cd969f68cdb73b7e4d299
Max <max@stoerchle.at>
2015-04-13 16:24:57 +0200

    weitergemacht

--- parseShanghaiRanking.rb
+++ parseShanghaiRanking.rb
@@ -1,26 +1,41 @@
-#encoding: UTF-8
+# encoding: UTF-8
 require 'rubygems'
+
+#parse xml/html
 require 'nokogiri'
+
+#open url
 require 'open-uri'
+
+@encoding = "ISO-8859-1"
+@currencies = {"€" => "EUR", "£" => "GBP", "$" => "USD"}
+@currenciesRegexStr = "["
+@currencies.each_pair do |k, v|
+  @currenciesRegexStr += "\\" + k.to_s
+end
+@currenciesRegexStr += ']\d+[\,\.]\d+'
+puts @currenciesRegexStr
+@currenciesRegexStr.force_encoding(@encoding)
+
 
 def getTuition(universityname)
   unistring = universityname.tr(' ', '+')
   link = 'https://www.google.at/search?q=' + unistring + '+tuition+total+per+year'
-  page = Nokogiri::HTML(open(link))
-  text = page.css('#search').text.force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
-  return text.match(/[\$\€\£]\d+[\,\.]\d+/).to_s
+  tuitionpage = Nokogiri::HTML(open(link))
+  text = tuitionpage.css('#search').text.force_encoding(@encoding)
+  return text.match(/#{@currenciesRegexStr}/).to_s
 end
 
 def convertToDollar(value)
   if value.length > 0
-    if value[0] == '$'
+    currencySign = value[0]
+    if currencySign == "$"
       return value
     else
-      link = 'https://www.google.at/search?q=' + value + ' to $'
-      # TODO
-      puts URI.escape(link)
-      page = Nokogiri::HTML(open(URI.escape(link)))
-      answer = page.css('div.vk_ans')[0].text
+      value.slice!(0)
+      link = "http://www.xe.com/currencyconverter/convert/?Amount=" + value.to_s + "&From=" + @currencies[currencySign].to_s + "&To=USD"
+      convertpage = Nokogiri::HTML(open(URI.escape(link)))
+      answer = convertpage.css('.uccRes .rightCol')[0].text
       return ("$" + answer)
     end
   else
